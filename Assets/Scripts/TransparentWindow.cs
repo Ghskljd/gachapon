62d4fe73349a68c2d9ede11a148bb51a5ff5980a
Ghskljd <ryannip86@gmail.com>
2025-08-26 00:37:04 -0600

    Created Transparent Application

--- Assets/Scripts/TransparentWindow.cs
+++ Assets/Scripts/TransparentWindow.cs
@@ -1,16 +1,90 @@
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewIndex);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll")]
+    public static extern bool SystemParametersInfo(int uaction, int uParam, ref Rect lpvParam, int fuWinIni);
+
+    private struct MARGINS
     {
-        
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
     }
 
-    // Update is called once per frame
-    void Update()
+    public struct Rect
     {
-        
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    const int GWL_EXSTYLE = -20;
+
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
+
+    const int SPI_GETWORKAREA = 0x0030;
+
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    IntPtr hWnd;
+    private void Start()
+    {
+//#if !UNITY_EDITOR
+        hWnd = GetActiveWindow();
+
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 };
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+        Rect workArea = new Rect();
+        SystemParametersInfo(SPI_GETWORKAREA, 0, ref workArea, 0);
+
+        int width = workArea.right - workArea.left;
+        int height = workArea.bottom - workArea.top;
+
+        SetWindowPos(hWnd, HWND_TOPMOST, workArea.left, workArea.top, width, height, 0);
+//#endif
+    }
+
+    private void Update()
+    {
+        UnityEngine.Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0f;
+        SetClickthrough(Physics2D.OverlapPoint(worldPos) == null);
+    }
+
+    private void SetClickthrough(bool clickthrough)
+    {
+        if (clickthrough)
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        else
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        }
     }
 }
