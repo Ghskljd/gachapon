f471041193dc75faa4429cef17d20f51928c9696
Ghskljd <ryannip86@gmail.com>
2025-09-08 22:07:46 -0600

    updated Assigning Page

--- Assets/Scripts/Buttons/ClickAssignCharacterButton.cs
+++ Assets/Scripts/Buttons/ClickAssignCharacterButton.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
@@ -11,12 +12,6 @@ public class ClickAssignCharacterButton : BuildingButtons
     [SerializeField]
     private GameObject CharacterDisplayPrefab;
     private List<GameObject> ListOfCharacterDisplay = new List<GameObject>();
-    private int commonIndex = 0;
-    private int uncommonIndex = 1;
-    private int rareIndex = 2;
-    private int ultraRareIndex = 3;
-    private int unicornRareIndex = 4;
-    private int burntRareIndex = 5;
     protected override void Awake()
     {
         base.Awake();
@@ -40,75 +35,27 @@ public class ClickAssignCharacterButton : BuildingButtons
 
     private void DisplayCharacters()
     {
-        foreach (KeyValuePair<Characters_SO, int> keyValuePair in CharacterListManager.Instance.PlayerCollection)
+        List<Characters_SO> keys = CharacterListManager.Instance.PlayerCollection.Keys.ToList(); //Get all the character_so in player collection
+        keys.Sort(); //sort the character_so by rarity
+        for (int i = 0; i < keys.Count; i++)
         {
+            if (!keys[i].unlocked) continue; //if the player hasn't unlocked dont display
+
+            //code below to create character display and set it in the correct order in the layout
             GameObject CreatedCharacterDisplay = Instantiate(CharacterDisplayPrefab);
             UnityEngine.UI.Image spriteRenderer = CreatedCharacterDisplay.GetComponent<UnityEngine.UI.Image>();
-            spriteRenderer.sprite = keyValuePair.Key.sprite;
+            spriteRenderer.sprite = keys[i].sprite;
             CreatedCharacterDisplay.transform.SetParent(CharacterLayoutGroup.transform, false);
-            FitToViewport(spriteRenderer);
-            switch (keyValuePair.Key.rarity)
-            {
-                default:
-                case Rarity.Common:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(commonIndex);
-                    commonIndex++;
-                    break;
-                case Rarity.Uncommon:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(commonIndex + 1);
-                    uncommonIndex = commonIndex + 1;
-                    break;
-                case Rarity.Rare:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(uncommonIndex + 1);
-                    rareIndex = uncommonIndex + 1;
-                    break;
-                case Rarity.UltraRare:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(rareIndex + 1);
-                    ultraRareIndex = rareIndex + 1;
-                    break;
-                case Rarity.UnicornRare:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(ultraRareIndex + 1);
-                    unicornRareIndex = ultraRareIndex + 1;
-                    break;
-                case Rarity.Burnt:
-                    CreatedCharacterDisplay.transform.SetSiblingIndex(unicornRareIndex + 1);
-                    burntRareIndex = unicornRareIndex + 1;
-                    break;
-            }
+            CreatedCharacterDisplay.transform.SetSiblingIndex(i);
 
+            //add to list of character display
             ListOfCharacterDisplay.Add(CreatedCharacterDisplay);
         }
-    }
-
-    public void FitToViewport(UnityEngine.UI.Image image)
-    {
-        RectTransform rt = image.rectTransform;
-
-        // Get the size of the viewport and the sprite
-        float viewportWidth = AssignCookiePage.GetComponent<RectTransform>().rect.width;
-        float viewportHeight = AssignCookiePage.GetComponent<RectTransform>().rect.height;
-
-        float spriteWidth = rt.rect.width;
-        float spriteHeight = rt.rect.height;
-
-        // Compute the scale factor to fit inside the viewport
-        float scaleX = viewportWidth / spriteWidth;
-        float scaleY = viewportHeight / spriteHeight;
-        float scale = Mathf.Min(scaleX, scaleY); // maintain aspect ratio
-
-        // Apply uniform scale
-        rt.localScale = new Vector3(scale, scale, 1f);
     }
 
     protected override void DisableButton()
     {
         base.DisableButton();
-        commonIndex = 0;
-        uncommonIndex = 1;
-        rareIndex = 2;
-        ultraRareIndex = 3;
-        unicornRareIndex = 4;
-        burntRareIndex = 5;
 
         foreach (GameObject characterDisplay in ListOfCharacterDisplay)
         {
--- Assets/Scripts/Characters_SO.cs
+++ Assets/Scripts/Characters_SO.cs
@@ -1,12 +1,19 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Character", menuName = "Character")]
-public class Characters_SO : BaseScriptableObject
+public class Characters_SO : BaseScriptableObject, IComparable<Characters_SO>
 {
     public Rarity rarity;
     public string Name;
     public Sprite sprite;
     public bool unlocked;
+
+    public int CompareTo(Characters_SO other)
+    {
+        if (other == null) return 1;
+        return rarity.CompareTo(other.rarity);
+    }
 }
 
 public enum Rarity
